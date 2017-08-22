eb1c6ba1dd349b8f044c7385b2034ecbf3c01446
Emmanuel BLONVIA <reallysmartguys@gmail.com>
2017-08-22 03:03:31 +0200

    ajout de material ui

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import AutoCompleteExampleDataSource from './common/Autocomplete'
 import ProspectForm from './client/ProspectForm'
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Accueil from './common/Accueil'
-
+import LoginForm from './common/login'
 import RaisedButton from 'material-ui/RaisedButton';
+import firebase from './lib/firebase';
 
 const style = {
     margin: 12,
@@ -20,8 +21,27 @@ class App extends Component {
         super(props);
         this.state = {
             key: 1,
-            ecole: ''
+            ecole: '',
+            signedIn: false
         };
+
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+                var providerData = user.providerData;
+                // ...
+            } else {
+                // User is signed out.
+                // ...
+            }
+        });
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.showProspectForm = this.showProspectForm.bind(this);
@@ -48,15 +68,16 @@ class App extends Component {
     }
 
     render() {
-
+let signedIn = this.state.signedIn
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h2>Bienvenu sur paillon</h2>
                 </div>
+                {!signedIn && <LoginForm/>}
+                {signedIn &&  <Accueil/>}
 
-                <Accueil/>
             </div>
         );
     }
